59b1cd39eb1e56d722caaf5f9f0f73fd59a56632
Abiola David <davidolanrewaju2000@gmail.com>
2023-06-23 15:27:05 +0100

    Created file to retrieve test subscription items

---
+++ app/src/main/java/com/example/buildspace/SubscriptionsList.kt
@@ -0,0 +1,71 @@
+package com.example.buildspace
+
+import com.example.buildspace.models.Subscription
+
+object SubscriptionsList {
+    val items = listOf(
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = true
+        ),
+
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = false
+        ),
+
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = true
+        ),
+
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = false
+        ),
+
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = true
+        ),
+
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = false
+        ),
+
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = true
+        ),
+
+        Subscription(
+            amount = 7000f,
+            type = "Monthly",
+            startDate = "03/01/23",
+            endDate = "03/29/23",
+            isSuccess = false
+        )
+    )
+}
