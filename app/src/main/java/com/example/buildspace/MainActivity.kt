6b67529c75559901fd66b0341080d138ed42f2c0
Abiola David <davidolanrewaju2000@gmail.com>
2023-06-27 15:30:16 +0100

    Conditional display of bottom navigation

--- app/src/main/java/com/example/buildspace/MainActivity.kt
+++ app/src/main/java/com/example/buildspace/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,14 +19,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.buildspace.models.BottomNavItem
-import com.example.buildspace.screens.Dashboard
-import com.example.buildspace.screens.SubscriptionHistory
-import com.example.buildspace.screens.SubscriptionPlans
+import com.example.buildspace.navigation.Navigation
+import com.example.buildspace.navigation.Screen
 import com.example.buildspace.ui.theme.BuildSpaceTheme
 import com.example.buildspace.ui.theme.LightBackground
 
@@ -34,60 +35,49 @@ class MainActivity : ComponentActivity() {
         setContent {
             BuildSpaceTheme {
                 val navController = rememberNavController()
+                val backStackEntry = navController.currentBackStackEntryAsState()
+                var showBottomBar by rememberSaveable{
+                    mutableStateOf(true)
+                }
+
+                showBottomBar = when(backStackEntry.value?.destination?.route) {
+                    Screen.SignInScreen.route -> false
+                    Screen.SignUpScreen.route -> false
+                    else -> true
+                }
                 Scaffold(
                     bottomBar = {
-                        BottomNavigationBar(
-                            items = listOf(
-                                BottomNavItem(
-                                    name = "Home",
-                                    route = "home",
-                                    icon = painterResource(id = R.drawable.outline_home_24)
-                                ),
+                        if (showBottomBar)
+                            BottomNavigationBar(
+                                items = listOf(
+                                    BottomNavItem(
+                                        name = "Home",
+                                        route = "home",
+                                        icon = painterResource(id = R.drawable.outline_home_24)
+                                    ),
 
-                                BottomNavItem(
-                                    name = "Subscriptions",
-                                    route = "subscriptions",
-                                    icon = painterResource(id = R.drawable.outline_subscriptions_24)
-                                ),
+                                    BottomNavItem(
+                                        name = "Subscriptions",
+                                        route = "subscriptions",
+                                        icon = painterResource(id = R.drawable.outline_subscriptions_24)
+                                    ),
 
-                                BottomNavItem(
-                                    name = "History",
-                                    route = "history",
-                                    icon = painterResource(id = R.drawable.outline_history_24)
-                                )
-                            ) ,
-                            navController = navController,
-                            onItemClick = {
-                                navController.navigate(it.route)
-                            }
-                        )
+                                    BottomNavItem(
+                                        name = "History",
+                                        route = "history",
+                                        icon = painterResource(id = R.drawable.outline_history_24)
+                                    )
+                                ),
+                                navController = navController,
+                                onItemClick = {
+                                    navController.navigate(it.route)
+                                }
+                            )
                     }
                 ) {
                     Navigation(navHostController = navController)
                 }
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                ) {
-//                    SignUp()
-//                }
             }
-        }
-    }
-}
-
-@Composable
-fun Navigation(navHostController: NavHostController){
-    NavHost(navController = navHostController, startDestination = "home"){
-        composable("home"){
-            Dashboard()
-
-        }
-        composable("subscriptions"){
-            SubscriptionPlans()
-
-        }
-        composable("history"){
-            SubscriptionHistory()
         }
     }
 }
@@ -100,6 +90,7 @@ fun BottomNavigationBar(
     onItemClick: (BottomNavItem) -> Unit
 ){
     val backStackEntry = navController.currentBackStackEntryAsState()
+
     NavigationBar(
         modifier = modifier,
         tonalElevation = 4.dp,
