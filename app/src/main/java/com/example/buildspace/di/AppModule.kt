a87be4edb2ab4851566437cba0667178d19170a9
Abiola David <davidolanrewaju2000@gmail.com>
2023-07-11 11:35:30 +0100

    Setup DI for TokenManager and AuthInterceptor

--- app/src/main/java/com/example/buildspace/di/AppModule.kt
+++ app/src/main/java/com/example/buildspace/di/AppModule.kt
@@ -1,15 +1,25 @@
 package com.example.buildspace.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.buildspace.data.local.TokenManager
 import com.example.buildspace.data.remote.Api
+import com.example.buildspace.data.remote.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 import javax.inject.Singleton
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -24,4 +34,13 @@ object AppModule {
             .build()
             .create()
     }
+
+    @Singleton
+    @Provides
+    fun provideTokenManager(@ApplicationContext context: Context): TokenManager = TokenManager(context)
+
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(tokenManager: TokenManager): AuthInterceptor =
+        AuthInterceptor(tokenManager)
 }
