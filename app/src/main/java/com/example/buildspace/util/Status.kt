36c2831aab74a1da1b8b9411eef9aac6cd8da33a
Abiola David <davidolanrewaju2000@gmail.com>
2023-07-17 15:55:04 +0100

    Helper method to get enum values

---
+++ app/src/main/java/com/example/buildspace/util/Status.kt
@@ -0,0 +1,25 @@
+package com.example.buildspace.util
+
+enum class Status {
+    PENDING,
+    CONFIRMED,
+    COMPLETED,
+    CANCELLED
+}
+
+enum class SubscriptionType{
+    DAILY,
+    WEEKLY,
+    MONTHLY
+}
+
+inline fun <reified T : Enum<T>> getEnumName(name: String): T {
+    check (isValidEnum<T>(name)){
+        throw IllegalStateException("Invalid ${T::class.simpleName} name")
+    }
+    return enumValueOf(name.uppercase())
+}
+
+inline fun <reified T : Enum<T>> isValidEnum(name: String): Boolean {
+    return enumValues<T>().any { enum -> enum.name.equals(name, ignoreCase = true) }
+}
