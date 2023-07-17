92f2932f354fac170479d5200ffc21e5a93d7455
Abiola David <davidolanrewaju2000@gmail.com>
2023-07-17 15:14:07 +0100

    refactored dateJoined type from LocalDateTime to String

--- app/src/main/java/com/example/buildspace/data/mapper/UserMapper.kt
+++ app/src/main/java/com/example/buildspace/data/mapper/UserMapper.kt
@@ -2,14 +2,8 @@ package com.example.buildspace.data.mapper
 
 import com.example.buildspace.data.remote.dto.response.UserDto
 import com.example.buildspace.domain.model.User
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
 
 fun UserDto.toUser(): User {
-    val pattern = "yyyy-MM-dd HH:mm:ss"
-    val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
-    val date = LocalDateTime.parse(dateJoined, formatter)
 
     return User(
         id = id,
@@ -17,7 +11,7 @@ fun UserDto.toUser(): User {
         lastName = lastName,
         email = email,
         phoneNumber = phoneNumber,
-        dateJoined = date,
+        dateJoined = dateJoined,
         role = role,
     )
 }
--- app/src/main/java/com/example/buildspace/domain/model/User.kt
+++ app/src/main/java/com/example/buildspace/domain/model/User.kt
@@ -1,13 +1,11 @@
 package com.example.buildspace.domain.model
 
-import java.time.LocalDateTime
-
 data class User(
     val id: String,
     val firstName: String,
     val lastName: String,
     val email: String,
     val phoneNumber: String,
-    val dateJoined: LocalDateTime,
+    val dateJoined: String,
     val role: String
 )
