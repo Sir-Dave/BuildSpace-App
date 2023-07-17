1d083a166638ae1a89d88acab9fd2aae33a3c83f
Abiola David <davidolanrewaju2000@gmail.com>
2023-07-17 14:18:57 +0100

    Implemented api methods for subscriptions

--- app/src/main/java/com/example/buildspace/data/remote/Api.kt
+++ app/src/main/java/com/example/buildspace/data/remote/Api.kt
@@ -4,9 +4,14 @@ import com.example.buildspace.data.remote.dto.request.RegisterRequest
 import com.example.buildspace.data.remote.dto.request.SignInRequest
 import com.example.buildspace.data.remote.dto.response.ApiResponse
 import com.example.buildspace.data.remote.dto.response.LoginResponse
+import com.example.buildspace.data.remote.dto.response.SubscriptionPlanDto
+import com.example.buildspace.data.remote.dto.response.SubscriptionDto
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface Api {
 
@@ -23,4 +28,25 @@ interface Api {
     suspend fun loginUser(
         @Body signInRequest: SignInRequest
     ): Response<LoginResponse>
+
+
+    @GET("api/v1/subscriptions")
+    suspend fun getAllSubscriptions(
+        @Query("userId") userId: String
+    ): Response<List<SubscriptionDto>>
+
+
+    @GET("api/v1/subscriptions/current")
+    suspend fun getCurrentSubscription(
+        @Query("userId") userId: String
+    ): Response<SubscriptionDto>
+
+    @GET("api/v1/subscriptions/id")
+    suspend fun getSubscriptionById(
+        @Path("id") id: String
+    ): Response<SubscriptionDto>
+
+    @GET("api/v1/subscriptions/plans")
+    suspend fun getAllSubscriptionPlans(): Response<SubscriptionPlanDto>
+
 }
---
+++ app/src/main/java/com/example/buildspace/data/repository/SubscriptionRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.buildspace.data.repository
+
+import com.example.buildspace.data.remote.Api
+import com.example.buildspace.data.remote.dto.response.SubscriptionDto
+import com.example.buildspace.data.remote.dto.response.SubscriptionPlanDto
+import com.example.buildspace.domain.repository.SubscriptionRepository
+import com.example.buildspace.util.Resource
+import com.example.buildspace.util.apiRequestFlow
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SubscriptionRepositoryImpl @Inject constructor(
+    private val api: Api) : SubscriptionRepository{
+
+    override suspend fun getAllUserSubscriptions(userId: String): Flow<Resource<List<SubscriptionDto>>> {
+        return apiRequestFlow {
+            api.getAllSubscriptions(userId)
+        }
+    }
+
+    override suspend fun getUserCurrentSubscription(userId: String): Flow<Resource<SubscriptionDto>> {
+        return apiRequestFlow {
+            api.getCurrentSubscription(userId)
+        }
+    }
+
+    override suspend fun getSubscriptionById(id: String): Flow<Resource<SubscriptionDto>> {
+        return apiRequestFlow {
+            api.getSubscriptionById(id)
+        }
+    }
+
+    override suspend fun getAllSubscriptionPlans(): Flow<Resource<SubscriptionPlanDto>> {
+        return apiRequestFlow {
+            api.getAllSubscriptionPlans()
+        }
+    }
+
+}
