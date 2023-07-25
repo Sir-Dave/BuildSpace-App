ef9f2f535007ccf712ba37ed53c1446d46d87bab
Abiola David <davidolanrewaju2000@gmail.com>
2023-07-25 03:42:36 +0100

    Added timestamp field to store time of cache

--- app/src/main/java/com/example/buildspace/data/mapper/SubscriptionMapper.kt
+++ app/src/main/java/com/example/buildspace/data/mapper/SubscriptionMapper.kt
@@ -1,5 +1,8 @@
 package com.example.buildspace.data.mapper
 
+import com.example.buildspace.data.local.SubscriptionEntity
+import com.example.buildspace.data.local.SubscriptionHistoryEntity
+import com.example.buildspace.data.local.SubscriptionPlanEntity
 import com.example.buildspace.data.remote.dto.response.SubscriptionDto
 import com.example.buildspace.data.remote.dto.response.SubscriptionHistoryDto
 import com.example.buildspace.data.remote.dto.response.SubscriptionPlanDto
@@ -31,7 +34,39 @@ fun SubscriptionDto.toSubscription(): Subscription{
         amount = String.format("%.2f", amount),
         expired = expired
     )
+}
 
+fun SubscriptionDto.toSubscriptionEntity(timestamp: Long): SubscriptionEntity{
+    return SubscriptionEntity(
+        id = id,
+        startDate = startDate,
+        endDate = endDate,
+        type = type,
+        amount = amount,
+        expired = expired,
+        timestamp = timestamp
+    )
+}
+
+fun SubscriptionEntity.toSubscription(): Subscription{
+    val inputFormat = "yyyy-MM-dd HH:mm:ss"
+    val outputFormat = "dd/MM/yyyy"
+    val inputFormatter = DateTimeFormatter.ofPattern(inputFormat, Locale.getDefault())
+    val outputFormatter = DateTimeFormatter.ofPattern(outputFormat, Locale.getDefault())
+
+    val localStartDateTime = LocalDateTime.parse(startDate, inputFormatter)
+    val localEndDateTime = LocalDateTime.parse(endDate, inputFormatter)
+    val formattedStartDate = localStartDateTime.format(outputFormatter)
+    val formattedEndDate = localEndDateTime.format(outputFormatter)
+
+    return Subscription(
+        id = id,
+        startDate = formattedStartDate,
+        endDate = formattedEndDate,
+        type = type,
+        amount = String.format("%.2f", amount),
+        expired = expired
+    )
 }
 
 fun SubscriptionHistoryDto.toSubscriptionHistory(): SubscriptionHistory {
@@ -60,11 +95,67 @@ fun SubscriptionHistoryDto.toSubscriptionHistory(): SubscriptionHistory {
     )
 }
 
+fun SubscriptionHistoryEntity.toSubscriptionHistory(): SubscriptionHistory {
+    val inputFormat = "yyyy-MM-dd HH:mm:ss"
+    val outputFormat = "dd/MM/yyyy"
+    val inputFormatter = DateTimeFormatter.ofPattern(inputFormat, Locale.getDefault())
+    val outputFormatter = DateTimeFormatter.ofPattern(outputFormat, Locale.getDefault())
+
+    val localDateTime = LocalDateTime.parse(date, inputFormatter)
+    val formattedDate = localDateTime.format(outputFormatter)
+
+    val statusType = getEnumName<Status>(status)
+
+    val formattedAmount = amount/100.0
+
+    return SubscriptionHistory(
+        id = id,
+        amount = String.format("%.2f", formattedAmount),
+        reference = reference,
+        date = formattedDate,
+        status = status,
+        userEmail = userEmail,
+        subscriptionType = subscriptionType,
+        currency = currency,
+        isSuccess = statusType != Status.CANCELLED
+    )
+}
+
+fun SubscriptionHistoryDto.toSubscriptionHistoryEntity(): SubscriptionHistoryEntity {
+    return SubscriptionHistoryEntity(
+        id = id,
+        amount = amount,
+        reference = reference,
+        date = date,
+        status = status,
+        userEmail = userEmail,
+        subscriptionType = subscriptionType,
+        currency = currency,
+    )
+}
+
 fun SubscriptionPlanDto.toSubscriptionPlan(): SubscriptionPlan{
     return SubscriptionPlan(
         name = name,
         amount = amount.toInt().toString(),
         numberOfDays = numberOfDays
     )
-
 }
+
+fun SubscriptionPlanEntity.toSubscriptionPlan(): SubscriptionPlan {
+    return SubscriptionPlan(
+        name = name,
+        amount = amount.toInt().toString(),
+        numberOfDays = numberOfDays
+    )
+}
+
+fun SubscriptionPlanDto.toSubscriptionPlanEntity(): SubscriptionPlanEntity{
+    return SubscriptionPlanEntity(
+        name = name,
+        amount = amount,
+        numberOfDays = numberOfDays
+    )
+}
+
+
