6079a94b6a341e0835a09bbbf466da1c168ece33
Abiola David <davidolanrewaju2000@gmail.com>
2023-07-26 16:33:07 +0100

    Handle network error when making network requests

--- app/src/main/java/com/example/buildspace/util/ApiRequestFlow.kt
+++ app/src/main/java/com/example/buildspace/util/ApiRequestFlow.kt
@@ -1,5 +1,8 @@
 package com.example.buildspace.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import com.example.buildspace.data.remote.dto.response.ApiResponse
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
@@ -9,7 +12,15 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withTimeoutOrNull
 import retrofit2.Response
 
-fun <T> apiRequestFlow(call: suspend () -> Response<T>): Flow<Resource<T>> = flow<Resource<T>> {
+fun <T> apiRequestFlow(
+    context: Context,
+    call: suspend () -> Response<T>
+): Flow<Resource<T>> = flow<Resource<T>> {
+    if (!isNetworkAvailable(context)) {
+        emit(Resource.Error(message = "No internet connection!"))
+        return@flow
+    }
+
     emit(Resource.Loading(true))
 
     withTimeoutOrNull(10000L) {
@@ -34,3 +45,21 @@ fun <T> apiRequestFlow(call: suspend () -> Response<T>): Flow<Resource<T>> = flo
         }
     } ?: emit(Resource.Error(message = "Timeout! Please try again."))
 }.flowOn(Dispatchers.IO)
+
+
+private fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+    if (connectivityManager != null) {
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val actNw =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        return actNw.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                actNw.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+    }
+    return false
+}
+
+
+
+
